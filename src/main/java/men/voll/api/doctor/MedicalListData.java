ccca2e95fd8cd91b523615834f1a6fb0dd5f191d
Christian <chriscar.sam@gmail.com>
2023-05-02 16:37:56 -0500

    Probando lista #2

--- src/main/java/men/voll/api/doctor/MedicalListData.java
+++ src/main/java/men/voll/api/doctor/MedicalListData.java
@@ -2,4 +2,7 @@ package men.voll.api.doctor;
 
 public record MedicalListData(String name, String speciality, String document, String email) {
 
+    public MedicalListData(Doctor doctor){
+        this(doctor.getName(), doctor.getSpecialty().toString(), doctor.getDocument(), doctor.getEmail());
+    }
 }
