3bc0d082370963a0e0e25ec6bbf817a00ab0f1a2
Christian <chriscar.sam@gmail.com>
2023-05-02 17:08:54 -0500

    Ordenacion #1

--- src/main/java/men/voll/api/controller/DoctorController.java
+++ src/main/java/men/voll/api/controller/DoctorController.java
@@ -8,6 +8,7 @@ import men.voll.api.doctor.MedicalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,7 +23,7 @@ public class DoctorController {
         medicalRepository.save(new Doctor(medicalRecordData));
     }
     @GetMapping
-    public Page<MedicalListData> medicalList(Pageable pageable){
+    public Page<MedicalListData> medicalList(@PageableDefault(size = 2) Pageable pageable){
         return medicalRepository.findAll(pageable).map(MedicalListData::new);
     }
 
