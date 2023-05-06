a2031bf9e19aac2bae175e8ff3a2403f0a70e31d
Christian <chriscar.sam@gmail.com>
2023-05-06 14:21:06 -0500

    Tratando el error 404 #2

--- src/main/java/men/voll/api/infra/ErrorManager.java
+++ src/main/java/men/voll/api/infra/ErrorManager.java
@@ -1,4 +1,15 @@
 package men.voll.api.infra;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class ErrorManager {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity dealingError404(){
+        return ResponseEntity.notFound().build();
+    }
 }
