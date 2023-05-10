4410d9b9968d7cd21f79c81e0cf8851930dd797e
Christian <chriscar.sam@gmail.com>
2023-05-10 16:27:44 -0500

    Obteniendo el Token #2

--- src/main/java/men/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/men/voll/api/infra/security/SecurityFilter.java
@@ -14,7 +14,11 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // Obtener el token del header
-        var token = request.getHeader("Authorization").replace("Bearer ","");
+        var token = request.getHeader("Authorization");
+        if (token == null || token == ""){
+            throw new RuntimeException("The token sent is not valid");
+        }
+        token = token.replace("Bearer ","");
         System.out.println(token);
         filterChain.doFilter(request, response);
     }
